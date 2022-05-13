926d1cadb3e1d12d88225d3cb0e69590aab7cab3
gyeonghun <erguono@gmail.com>
2022-05-13 14:13:37 +0900

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+const fs = require("fs");
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(fs.readFileSync("index.html"));
+  res.end();
+});
+
+app.listen(3000, () => {
+  console.log("server listen 3000");
+});
